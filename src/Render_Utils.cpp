352e6441bd3fa19631d6a54c8bc340d2135e523c
Krystian Gryczon <me@example.com>
2021-01-20 20:49:09 +0100

    PhysX added

--- src/Render_Utils.cpp
+++ src/Render_Utils.cpp
@@ -5,6 +5,47 @@
 #include "glew.h"
 #include "freeglut.h"
 
+void Core::RenderContext::initFromOBJ(obj::Model& model)
+{
+	vertexArray = 0;
+	vertexBuffer = 0;
+	vertexIndexBuffer = 0;
+	unsigned int vertexDataBufferSize = sizeof(float) * model.vertex.size();
+	unsigned int vertexNormalBufferSize = sizeof(float) * model.normal.size();
+	unsigned int vertexTexBufferSize = sizeof(float) * model.texCoord.size();
+
+	size = model.faces["default"].size();
+	unsigned int vertexElementBufferSize = sizeof(unsigned short) * size;
+
+
+	glGenVertexArrays(1, &vertexArray);
+	glBindVertexArray(vertexArray);
+
+
+	glGenBuffers(1, &vertexIndexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, vertexElementBufferSize, &model.faces["default"][0], GL_STATIC_DRAW);
+
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+
+	glBufferData(GL_ARRAY_BUFFER, vertexDataBufferSize + vertexNormalBufferSize + vertexTexBufferSize, NULL, GL_STATIC_DRAW);
+
+	glBufferSubData(GL_ARRAY_BUFFER, 0, vertexDataBufferSize, &model.vertex[0]);
+
+	glBufferSubData(GL_ARRAY_BUFFER, vertexDataBufferSize, vertexNormalBufferSize, &model.normal[0]);
+
+	glBufferSubData(GL_ARRAY_BUFFER, vertexDataBufferSize + vertexNormalBufferSize, vertexTexBufferSize, &model.texCoord[0]);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)(0));
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)(vertexDataBufferSize));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)(vertexNormalBufferSize + vertexDataBufferSize));
+}
+
 void Core::DrawVertexArray(const float * vertexArray, int numVertices, int elementSize )
 {
 	glVertexAttribPointer(0, elementSize, GL_FLOAT, false, 0, vertexArray);
@@ -44,4 +85,16 @@ void Core::DrawModel( obj::Model * model )
 
 	unsigned short * tmp = &model->faces["default"][0];
 	glDrawElements(GL_TRIANGLES, model->faces["default"].size(), GL_UNSIGNED_SHORT, tmp);
+}
+void Core::DrawContext(Core::RenderContext& context)
+{
+
+	glBindVertexArray(context.vertexArray);
+	glDrawElements(
+		GL_TRIANGLES,      // mode
+		context.size,    // count
+		GL_UNSIGNED_SHORT,   // type
+		(void*)0           // element array buffer offset
+	);
+	glBindVertexArray(0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "Render_Utils.h"
 #include "Camera.h"
 #include "Texture.h"
+#include "Physics.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -23,8 +24,10 @@ GLuint programSkybox;
 GLuint programStatic;
 GLuint programExplode;
 
-const float RADIUS = 100.f;
-const int ASTEROIDS_NUMBER = 60;
+Core::RenderContext shipContext;
+
+const float RADIUS = 200.f;
+const int ASTEROIDS_NUMBER = 200;
 
 std::vector<GLuint> asteroidTextures;
 std::vector<obj::Model> asteroidModels;
@@ -38,8 +41,7 @@ GLuint skyboxVAO, skyboxVBO;
 
 Core::Shader_Loader shaderLoader;
 
-obj::Model shipModel;
-obj::Model sphereModel;
+obj::Model shipModel, sphereModel;
 
 
 glm::vec3 cameraPos = glm::vec3(0, 0, 0);
@@ -53,6 +55,86 @@ glm::vec3 lightDir = glm::normalize(glm::vec3(1.0f, -0.9f, -1.0f));
 
 glm::quat rotation = glm::quat(1, 0, 0, 0);
 
+//PhysX init
+Physics pxScene(0.f);
+// fixed timestep for stable and deterministic simulation
+const double physicsStepTime = 1.f / 60.f;
+double physicsTimeToProcess = 0;
+
+// physical objects
+PxRigidStatic *asteroidBody = nullptr;
+PxMaterial *asteroidMaterial = nullptr;
+PxRigidDynamic *shipBody = nullptr;
+PxMaterial *shipMaterial = nullptr;
+
+// renderable objects (description of a single renderable instance)
+struct Renderable {
+	Core::RenderContext* context;
+	glm::mat4 modelMatrix;
+	GLuint textureId;
+};
+std::vector<Renderable*> renderables;
+
+
+void initRenderables()
+{
+	Renderable *ship = new Renderable();
+	ship->context = &shipContext;
+	ship->textureId = textureShip;
+	renderables.emplace_back(ship);
+
+	// load models
+	shipModel = obj::loadModelFromFile("models/spaceship_cruiser.obj");
+	shipContext.initFromOBJ(shipModel);
+
+	// load textures
+	textureShip = Core::LoadTexture("textures/ship/cruiser01_diffuse.png");
+
+}
+
+void initPhysicsScene()
+{
+	shipMaterial = pxScene.physics->createMaterial(1, 1, 1);
+	PxRigidDynamic* shipBody = pxScene.physics->createRigidDynamic(PxTransform(cameraPos.x, cameraPos.y, cameraPos.z));
+	PxShape* shipShape = pxScene.physics->createShape(PxSphereGeometry(2.f), *shipMaterial);
+	shipBody->attachShape(*shipShape);
+	shipShape->release();
+	shipBody->userData = renderables[0];
+	pxScene.scene->addActor(*shipBody);
+}
+
+void updateTransforms()
+{
+	// Here we retrieve the current transforms of the objects from the physical simulation.
+	auto actorFlags = PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC;
+	PxU32 nbActors = pxScene.scene->getNbActors(actorFlags);
+	if (nbActors)
+	{
+		std::vector<PxRigidActor*> actors(nbActors);
+		pxScene.scene->getActors(actorFlags, (PxActor**)&actors[0], nbActors);
+		for (auto actor : actors)
+		{
+			// We use the userData of the objects to set up the model matrices
+			// of proper renderables.
+			if (!actor->userData) continue;
+			Renderable *renderable = (Renderable*)actor->userData;
+
+			// get world matrix of the object (actor)
+			PxMat44 transform = actor->getGlobalPose();
+			auto &c0 = transform.column0;
+			auto &c1 = transform.column1;
+			auto &c2 = transform.column2;
+			auto &c3 = transform.column3;
+
+			// set up the model matrix used for the rendering
+			renderable->modelMatrix = glm::mat4(
+				c0.x, c0.y, c0.z, c0.w,
+				c1.x, c1.y, c1.z, c1.w,
+				c2.x, c2.y, c2.z, c2.w,
+				c3.x, c3.y, c3.z, c3.w);
+		}
+	}
+}
 
 bool firstMouse;
 float newMouseX = 0;
@@ -168,8 +250,28 @@ void drawObjectTexture(obj::Model * model, glm::mat4 modelMatrix, GLuint texture
 	GLuint program = programTexture;
 
 	glUseProgram(program);
-	float t = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
-	lightDir = glm::vec3(lightDir.x, lightDir.y, lightDir.z);
+
+	glUniform3f(glGetUniformLocation(program, "lightDir"), lightDir.x, lightDir.y, lightDir.z);
+	glUniform3f(glGetUniformLocation(program, "cameraPos"), cameraPos.x, cameraPos.y, cameraPos.z);
+
+	glm::mat4 transformation = perspectiveMatrix * cameraMatrix * modelMatrix;
+	glUniformMatrix4fv(glGetUniformLocation(program, "modelViewProjectionMatrix"), 1, GL_FALSE, (float*)&transformation);
+	glUniformMatrix4fv(glGetUniformLocation(program, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
+
+	Core::SetActiveTexture(textureId, "textureSampler", program, 0);
+	//Core::SetActiveTexture(normalmapId, "normalSampler", program, 1);
+
+	Core::DrawModel(model);
+
+	glUseProgram(0);
+}
+
+void drawObjectTextureFromContext(Core::RenderContext * context, glm::mat4 modelMatrix, GLuint textureId)
+{
+	GLuint program = programTexture;
+
+	glUseProgram(program);
+
 	glUniform3f(glGetUniformLocation(program, "lightDir"), lightDir.x, lightDir.y, lightDir.z);
 	Core::SetActiveTexture(textureId, "textureSampler", program, 0);
 
@@ -177,7 +279,7 @@ void drawObjectTexture(obj::Model * model, glm::mat4 modelMatrix, GLuint texture
 	glUniformMatrix4fv(glGetUniformLocation(program, "modelViewProjectionMatrix"), 1, GL_FALSE, (float*)&transformation);
 	glUniformMatrix4fv(glGetUniformLocation(program, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
 
-	Core::DrawModel(model);
+	Core::DrawContext(*context);
 
 	glUseProgram(0);
 }
@@ -205,6 +307,20 @@ void drawObjectExplode(obj::Model* model, glm::mat4 modelMatrix, GLuint textureI
 
 void renderScene()
 {	
+	double time = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+	static double prevTime = time;
+	double dtime = time - prevTime;
+	prevTime = time;
+
+	// Update physics
+	if (dtime < 1.f) {
+		physicsTimeToProcess += dtime;
+		while (physicsTimeToProcess > 0) {
+			// here we perform the physics simulation step
+			pxScene.step(physicsStepTime);
+			physicsTimeToProcess -= physicsStepTime;
+		}
+	}
 	// Aktualizacja macierzy widoku i rzutowania
 	cameraMatrix = createCameraMatrix();
 	perspectiveMatrix = Core::createPerspectiveMatrix();
@@ -216,16 +332,14 @@ void renderScene()
 	glm::mat4 shipInitialTransformation = glm::translate(glm::vec3(0, -0.25f, 0)) * glm::rotate(glm::radians(180.0f), glm::vec3(0, 1, 0)) * glm::scale(glm::vec3(0.25f));
 	glm::mat4 shipModelMatrix = glm::translate(cameraPos + cameraDir * 0.5f) * glm::mat4_cast(glm::inverse(rotation)) * shipInitialTransformation;
 	
-	
-
-	if (!explode) {
-		drawObjectExplode(&shipModel, shipModelMatrix* glm::scale(glm::vec3(0.075f)), textureShip);
-		expl_time = 0.0;
-	}
-	else if (expl_time <= 2.0){
-		drawObjectExplode(&shipModel, shipModelMatrix* glm::scale(glm::vec3(0.075f)), textureShip);
-	}	
-
+	//if (!explode) {
+	//	drawObjectExplode(&shipModel, shipModelMatrix* glm::scale(glm::vec3(0.075f)), textureShip);
+	//	expl_time = 0.0;
+	//}
+	//else if (expl_time <= 2.0){
+	//	drawObjectExplode(&shipModel, shipModelMatrix* glm::scale(glm::vec3(0.075f)), textureShip);
+	//}	
+	drawObjectTextureFromContext(renderables[0]->context, shipModelMatrix* glm::scale(glm::vec3(0.075f)), renderables[0]->textureId);
 
 	for (Asteroid asteroid : asteroids) {
 
@@ -275,16 +389,17 @@ void init()
 	programSkybox = shaderLoader.CreateProgram("shaders/shader_skybox.vert", "shaders/shader_skybox.frag");
 	programStatic = shaderLoader.CreateProgram("shaders/shader_hp.vert", "shaders/shader_hp.frag");
 	programExplode = shaderLoader.CreateProgram("shaders/shader_explode.vert", "shaders/shader_explode.frag", "shaders/shader_explode.geom");
-	sphereModel = obj::loadModelFromFile("models/sphere.obj");
-	shipModel = obj::loadModelFromFile("models/spaceship_cruiser.obj");
-	textureShip = Core::LoadTexture("textures/ship/cruiser01_diffuse.png");
-	textureShipNormal = Core::LoadTexture("textures/ship/cruiser01_specular.png");
+	//sphereModel = obj::loadModelFromFile("models/sphere.obj");
+	//shipModel = obj::loadModelFromFile("models/spaceship_cruiser.obj");
+	/*textureShip = Core::LoadTexture("textures/ship/cruiser01_diffuse.png");*/
+	textureShipNormal = Core::LoadTexture("textures/ship/cruiser01_secular.png");
 
 	firstMouse = true;
 	
 	cubemapTexture = Skybox::loadCubemap(faces);
 	initAsteroids();
-
+	initRenderables();
+	initPhysicsScene();
 	//Core::setA(1);
 	//std::cout << Core::getA();
 }
@@ -305,6 +420,7 @@ void idle()
 
 void onReshape(int width, int height)
 {	
+	//Core::setFrustumScale((float)width / height);
 	glViewport(0, 0, width, height);
 }
 
@@ -314,7 +430,7 @@ int main(int argc, char ** argv)
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(200, 200);
 	glutInitWindowSize(600, 600);
-	glutCreateWindow("OpenGL Pierwszy Program");
+	glutCreateWindow("Space shitter");
 	glewInit();
 	glutSetCursor(GLUT_CURSOR_NONE);
 	init();
